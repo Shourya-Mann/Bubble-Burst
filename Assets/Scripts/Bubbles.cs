60f0d8e42f82ee1e316499cc680a2df09b37a744
shourya mann <sm4836@rit.edu>
2023-06-05 11:08:02 -0400

    Comments for future coding
    Wrote out the logic for basic gameplay including interaction, refil and static states. also for checks of angles and color for state changes and when to allow the bubbles to burst.
    
    NOTE: currently written as comments as new functions, methods and if-else statments need to be written

--- Assets/Scripts/Bubbles.cs
+++ Assets/Scripts/Bubbles.cs
@@ -26,14 +26,46 @@ public class Bubbles : MonoBehaviour
     // Lets record our input
     private void OnMouseDown()
     {
-        firstTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+        firstTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         // Screen to world point just changes unity default of pixel coordinates to our world coordinates fo row, column
+
+
+        // check what game state ur in
+        // if board reffil state -----> wait (no interaction)
+        // else if in interaction state(mouse up timer is running) ------> reset timer ----> continue interaction state
+        // if board filled state -----> begin interaction state
+
+
+        //check for color correctness
+        // if right bubble -----> break ----> initiate haptic----> generate points
+        // else ------> end interaction state --------> enter board refill state
+
+
+        // store the first postion as the prev postion to calculate the angle
+        prevTouchPos = firstTouchPos;
+
+
+
     }
 
-    private void OnMouseEnter()
+    private void OnMouseEnter() // for when player drags to burst bubbles in combo
     {
+
         currentTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        // check angle
+        // if angle = 0, 90, 180, 270 --------> go to color check and
+                // if right bubble -----> break ----> initiate haptic----> generate points
+                // else ------> end interaction state --------> enter board refill state
+        // else --------> end interaction state --------> enter board refill state
+
+        //NOTE: for color check, if true points increase for longer drag. (need a counter for nuber of bubbles burst in combo)
+
+
+
+
+        /*
+         NOTE: THIS ANGLE CALCULATION NEEDS TO BE MODIFIED
         if (prevTouchPos == firstTouchPos)
         {
             // Initial drag, calculate angle from firstTouchPos to currentTouchPos
@@ -44,12 +76,22 @@ public class Bubbles : MonoBehaviour
             // Subsequent drag, calculate angle from prevTouchPos to currentTouchPos
             CalculateAngle(prevTouchPos, currentTouchPos);
         }
+        */
+
+        // update the current touch pos to be the prev touch pos so the angle is calculated for the next drag
+        prevTouchPos = currentTouchPos;
 
     }
 
     private void OnMouseUp()
     {
-        finalTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // store earned points in counter 
+
+        // begin hidden timer
+        // if timer ends -----> end interaction state --------> enter board refill state
+        // if mouse down before timer ends ------> reset timer ////// NOTE: might need to make a seperate func as timer
+
     }
 
     void CalculateAngle(Vector2 fromPos, Vector2 ToPos)
