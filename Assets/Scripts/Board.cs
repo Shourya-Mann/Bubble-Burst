68711531682aa0cf01b8cda88284e7450329862c
shourya mann <sm4836@rit.edu>
2023-06-07 12:17:02 -0400

    DESTROY!!?
    built the bubble destruction mechanism for OnMouseDown and OnMouseEnter.
    Had to nest OnmouseEnter in OnMouseDown to prevent destruction from just hovering over the board.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -26,7 +27,7 @@ public class Board : MonoBehaviour
             for (int column = 0; column < height; column++)
             {
                 Vector2 tempPos = new Vector2(row, column);
-                GameObject BackTile =  Instantiate(tilePrefab, tempPos, Quaternion.identity) as GameObject;
+                GameObject BackTile = Instantiate(tilePrefab, tempPos, Quaternion.identity) as GameObject;
                 BackTile.transform.parent = this.transform;
                 // lets also get the postions for the BackTile for sanity Checks
                 BackTile.name = "(" + row + " ," + column + ")";
@@ -38,5 +39,64 @@ public class Board : MonoBehaviour
             }
         }
     }
+
+    public void BreakBubble()
+    {
+        // Destroy the bubble GameObject
+        Destroy(gameObject);
+    }
 }
 
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    public GameObject[] bubbles;
+    private BackgroundTile[,] allTiles;
+    public GameObject[,] allBubble;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height];
+        allBubble = new GameObject[width, height];
+        Setup();
+    }
+
+    private void Setup()
+    {
+        for (int row = 0; row < width; row++)
+        {
+            for (int column = 0; column < height; column++)
+            {
+                Vector2 tempPos = new Vector2(row, column);
+                GameObject backTile = Instantiate(tilePrefab, tempPos, Quaternion.identity);
+                backTile.transform.parent = transform;
+                backTile.name = "(" + row + ", " + column + ")";
+
+                int toUse = Random.Range(0, bubbles.Length);
+                GameObject bubble = Instantiate(bubbles[toUse], tempPos, Quaternion.identity);
+                bubble.transform.parent = backTile.transform;
+                bubble.name = "(" + row + ", " + column + ")";
+                allBubble[row, column] = bubble;
+            }
+        }
+    }
+
+    public void BreakBubble(float column, float row)
+    {
+        // Destroy the bubble GameObject
+        int columnIndex = Mathf.RoundToInt(column);
+        int rowIndex = Mathf.RoundToInt(row);
+        GameObject bubbleObject = allBubble[columnIndex, rowIndex];
+        Destroy(bubbleObject);
+        bubbleObject = null;
+        Debug.Log("Bubble Destroyed!");
+    }
+}
--- Assets/Scripts/Bubbles.cs
+++ Assets/Scripts/Bubbles.cs
@@ -10,11 +10,12 @@ public class Bubbles : MonoBehaviour
     private Vector2 currentTouchPos;
     private Vector2 prevTouchPos;
     public float swipeAngle = 0;
-
+    private Board board;
+    //private bool isMousePressed = false; // boolean flag to check if moue is pressed (we dont want bubbles bursting randomly by hovering the mouse
     // Start is called before the first frame update
     void Start()
     {
-        
+        board = FindObjectOfType<Board>(); // find the Board script in the scene
     }
 
     // Update is called once per frame
@@ -26,6 +27,8 @@ public class Bubbles : MonoBehaviour
     // Lets record our input
     private void OnMouseDown()
     {
+        Debug.Log("Mouse down on bubble!");
+        //isMousePressed = true;
         firstTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         // Screen to world point just changes unity default of pixel coordinates to our world coordinates fo row, column
 
@@ -39,53 +42,51 @@ public class Bubbles : MonoBehaviour
         //check for color correctness
         // if right bubble -----> break ----> initiate haptic----> generate points
         // else ------> end interaction state --------> enter board refill state
-
+        board.BreakBubble(firstTouchPos.x, firstTouchPos.y);
 
         // store the first postion as the prev postion to calculate the angle
+
+
+
         prevTouchPos = firstTouchPos;
 
+        // NESTING ONMOUSE ENTER for now
+            // NESTING ONMOUSEENTER
+        OnMouseEnter();
 
+        
 
     }
-
-    private void OnMouseEnter() // for when player drags to burst bubbles in combo
+ 
+    void OnMouseEnter() // for when player drags to burst bubbles in combo
     {
-
-        currentTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        // check angle
-        // if angle = 0, 90, 180, 270 --------> go to color check and
-                // if right bubble -----> break ----> initiate haptic----> generate points
-                // else ------> end interaction state --------> enter board refill state
-        // else --------> end interaction state --------> enter board refill state
-
-        //NOTE: for color check, if true points increase for longer drag. (need a counter for nuber of bubbles burst in combo)
-
-
-
-
-        /*
-         NOTE: THIS ANGLE CALCULATION NEEDS TO BE MODIFIED
-        if (prevTouchPos == firstTouchPos)
+        if (Input.GetMouseButton(0))
         {
-            // Initial drag, calculate angle from firstTouchPos to currentTouchPos
-            CalculateAngle(firstTouchPos, currentTouchPos);
-        }
-        else
-        {
-            // Subsequent drag, calculate angle from prevTouchPos to currentTouchPos
-            CalculateAngle(prevTouchPos, currentTouchPos);
-        }
-        */
+            currentTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        // update the current touch pos to be the prev touch pos so the angle is calculated for the next drag
-        prevTouchPos = currentTouchPos;
+            // check angle
+            // if angle = 0, 90, 180, 270 --------> go to color check and
+            // if right bubble -----> break ----> initiate haptic----> generate points
+            // else ------> end interaction state --------> enter board refill state
+            // else --------> end interaction state --------> enter board refill state
 
+            //NOTE: for color check, if true points increase for longer drag. (need a counter for nuber of bubbles burst in combo)
+            board.BreakBubble(currentTouchPos.x, currentTouchPos.y);
+
+
+
+
+
+            // update the current touch pos to be the prev touch pos so the angle is calculated for the next drag
+            prevTouchPos = currentTouchPos;
+        }
     }
+
+
 
     private void OnMouseUp()
     {
-
+        //isMousePressed = false;
         // store earned points in counter 
 
         // begin hidden timer
@@ -101,3 +102,6 @@ public class Bubbles : MonoBehaviour
         swipeAngle = Mathf.Atan2(currentTouchPos.y - firstTouchPos.y, currentTouchPos.x - firstTouchPos.x);
     }
 }
+
+
+
