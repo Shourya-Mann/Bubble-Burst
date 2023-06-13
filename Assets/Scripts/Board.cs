2604ca3248296bb1b446ef8f46c44565fd798d79
shourya mann <sm4836@rit.edu>
2023-06-13 08:53:50 -0400

    Gamestates and Refill the board
    Set up the initial names for the game states and built the Coroutine to refill the board. (will fix the compile errors later)

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,56 +1,13 @@
-/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour
+public enum GameState// it can have certain different states for this game we have wait and move but wecan create stuff like booster on etc.
 {
-
-    public int width;
-    public int height;
-    public GameObject tilePrefab;
-    public GameObject[] bubbles;
-    private BackgroundTile[,] allTiles;
-    public GameObject[,] allBubble;
-    // Start is called before the first frame update
-    void Start()
-    {
-        allTiles = new BackgroundTile[width, height];
-        allBubble = new GameObject[width, height];
-        Setup();
-    }
-
-    private void Setup()
-    {
-        for (int row = 0; row < width; row++)
-        {
-            for (int column = 0; column < height; column++)
-            {
-                Vector2 tempPos = new Vector2(row, column);
-                GameObject BackTile = Instantiate(tilePrefab, tempPos, Quaternion.identity) as GameObject;
-                BackTile.transform.parent = this.transform;
-                // lets also get the postions for the BackTile for sanity Checks
-                BackTile.name = "(" + row + " ," + column + ")";
-                int toUse = Random.Range(0, bubbles.Length);
-                GameObject bubble = Instantiate(bubbles[toUse], tempPos, Quaternion.identity);
-                bubble.transform.parent = this.transform; // bubble is the child of the backtile
-                bubble.name = "(" + row + " ," + column + ")"; // sanity check
-                allBubble[row, column] = bubble;
-            }
-        }
-    }
-
-    public void BreakBubble()
-    {
-        // Destroy the bubble GameObject
-        Destroy(gameObject);
-    }
+    refill,// set up in candy class
+    play // set up in this class
+    
 }
-
-*/
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public class Board : MonoBehaviour
 {
@@ -96,7 +53,63 @@ public class Board : MonoBehaviour
         int rowIndex = Mathf.RoundToInt(row);
         GameObject bubbleObject = allBubble[columnIndex, rowIndex];
         Destroy(bubbleObject);
-        bubbleObject = null;
+        //bubbleObject = null;
         Debug.Log("Bubble Destroyed!");
     }
+
+    
+    private IEnumerator RefillBoardCoroutine()
+    {
+        // Wait for a short delay (optional, for visual effect)
+        yield return new WaitForSeconds(0.5f);
+
+        // Loop through each column
+        for (int columnIndex = 0; columnIndex < height; columnIndex++)
+        {
+            // Create a list to store the bubbles that need to move down
+            List<GameObject> bubblesToMove = new List<GameObject>();
+
+            // Loop through each row from bottom to top
+            for (int rowIndex = 0; rowIndex < width; rowIndex++)
+            {
+                // Check if the current slot is empty
+                if (allBubble[columnIndex, rowIndex] == null)
+                {
+                    // Add the bubbles above the empty slot to the list
+                    for (int i = rowIndex + 1; i < width; i++)
+                    {
+                        GameObject bubble = allBubble[columnIndex, i];
+                        if (bubble != null)
+                        {
+                            bubblesToMove.Add(bubble);
+                            allBubble[columnIndex, i] = null;
+                        }
+                    }
+                }
+            }
+
+            // Move the bubbles down
+            for (int i = 0; i < bubblesToMove.Count; i++)
+            {
+                int targetRow = width - 1 - i;
+                allBubble[columnIndex, targetRow] = bubblesToMove[i];
+                bubblesToMove[i].transform.position = GetPosition(columnIndex, targetRow);
+            }
+
+            // Refill the remaining empty slots with new bubbles
+            for (int rowIndex = 0; rowIndex < width; rowIndex++)
+            {
+                if (allBubble[columnIndex, rowIndex] == null)
+                {
+                    RefillSlot(columnIndex, rowIndex);
+                }
+            }
+        }
+
+        // Optional: Wait for a short delay before enabling player input
+        yield return new WaitForSeconds(0.5f);
+
+        // Enable player input or perform other necessary actions
+    }
+    
 }
